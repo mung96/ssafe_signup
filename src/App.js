172f0e0d0cd14b860145cdd2d382cb17ef010688
mung5079 <mung5079@gmail.com>
2024-02-26 17:57:37 +0900

    feat: GlobalHeader 레이아웃 잡기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import SignUpPage from "./pages/SignUpPage";
-
+import { GlobalHeader } from "./components/common/globalheader/GlobalHeader";
 
 function App() {
-  return <Routes>
+  return <>
+    <GlobalHeader />
+    <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/signup" element={<SignUpPage />} />
     </Routes>
-  ;
+  </>;
 }
 
 export default App;
